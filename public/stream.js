c5236b3ca79a474bb0e86aca11dc33eaad434314
Derek Lindahl <dlindahl@customink.com>
2013-08-13 15:06:07 -0400

    Adds multi-application support to example page

--- public/stream.js
+++ public/stream.js
@@ -1,50 +1,114 @@
 (function() {
-  var source = null;
+  var TownCrier = function(options) {
+    this.options = options || {};
 
-  function onOpen() {
-    document.getElementById('status').textContent = 'Connected';
-    document.getElementById('connection').textContent = 'Disconnect';
-  }
+    this.connect();
+  };
 
-  function onMessage(e) {
-    console.log(e.data);
-    var stream = document.getElementById('stream');
-    stream.value += e.data + "\n";
-    stream.scrollTop = stream.scrollHeight;
-  }
-
-  function onError(e) {
-    document.getElementById('connection').textContent = 'Connect';
-    var status = 'Connection closed.';
-    if (e.readyState === EventSource.CLOSED) {
-      source.close();
-    } else if (e.readyState === EventSource.CONNECTING) {
-      status += ' Attempting to reconnect!';
-    } else {
-      status += ' Unknown error!';
-    }
-    document.getElementById('status').textContent = status;
-  }
-
-  function connect() {
-    source = new EventSource('/firehose/sap/note.%23.orders.%23' + window.location.search);
-
-    source.addEventListener('open', onOpen, false);
-    source.addEventListener('message', onMessage, false);
-    source.addEventListener('error', onError, false);
-  }
-
-  document.getElementById('connection').addEventListener('click', function() {
-    var label = this.textContent;
-    if(label == 'Connect') {
-      connect();
-    } else if(label == 'Disconnect') {
-      if(source) {
-        source.close();
-        onError(source);
+  function cbRunner(tc, name) {
+    return function() {
+      if(tc.options[name]) {
+        tc.options[name].apply(tc, arguments);
       }
-    }
+    };
+  }
+  function errorHandler(tc) {
+    return function(e) {
+      if (e.readyState === EventSource.CLOSED) {
+        this.source.close();
+        cbRunner(tc,'onClose').apply(tc, arguments);
+      } else if (e.readyState === EventSource.CONNECTING) {
+        cbRunner(tc,'onReconnect').apply(tc, arguments);
+      } else {
+        var cb = cbRunner(tc,'onError');
+        if(cb) { cb.apply(tc, arguments); }
+      }
+    };
+  }
+
+  TownCrier.prototype.connect = function() {
+    var url = '/firehose/'+this.options.appName+'/'+this.options.key + window.location.search;
+
+    console.log(url);
+
+    this.source = new EventSource(url);
+    this.source.addEventListener('open', cbRunner(this,'onOpen'), false);
+    this.source.addEventListener('message', cbRunner(this,'onMessage'), false);
+    this.source.addEventListener('error', errorHandler(this), false);
+  };
+
+  window.TownCrier = TownCrier;
+
+  function onOpen(prefix) {
+    return function() {
+      document.getElementById(prefix+'Status').textContent = 'Connected';
+      document.getElementById(prefix+'Connection').textContent = 'Disconnect';
+    };
+  }
+  function onMessage(prefix) {
+    return function(e) {
+      console.log(prefix, e.data);
+      var stream = document.getElementById(prefix+'Stream');
+      stream.value += e.data + "\n";
+      stream.scrollTop = stream.scrollHeight;
+    };
+  }
+  function onReconnect(prefix) {
+    return function() {
+      document.getElementById(prefix+'Connection').textContent = 'Connect';
+      document.getElementById(prefix+'Status').textContent = 'Reconnecting';
+    };
+  }
+  function onClose(prefix) {
+    return function() {
+      document.getElementById(prefix+'Connection').textContent = 'Connect';
+      document.getElementById(prefix+'Status').textContent = 'Connection closed';
+    };
+  }
+  function onError(prefix) {
+    return function() {
+      document.getElementById(prefix+'Connection').textContent = 'Connect';
+      document.getElementById(prefix+'Status').textContent = 'Connection Error!';
+    };
+  }
+
+  var sap = new TownCrier({
+    appName:'sap',
+    key:'note.%23.orders.%23',
+    onOpen : onOpen('sap'),
+    onMessage : onMessage('sap'),
+    onReconnect : onReconnect('sap'),
+    onClose : onClose('sap'),
+    onError : onError('sap')
   });
 
-  connect();
+  var qct = new TownCrier({
+    appName:'qct',
+    key:'%23',
+    onOpen : onOpen('qct'),
+    onMessage : onMessage('qct'),
+    onReconnect : onReconnect('qct'),
+    onClose : onClose('qct'),
+    onError : onError('qct')
+  });
+
+  var onClick = function(tc) {
+    return function() {
+      var label = this.textContent;
+      if(label == 'Connect') {
+        tc.connect();
+      } else if(label == 'Disconnect') {
+        if(tc.source) {
+          tc.source.close();
+          tc.options.onClose.call(tc);
+        }
+      }
+    };
+  };
+
+  document.getElementById('sapConnection').addEventListener('click', onClick(sap));
+  document.getElementById('qctConnection').addEventListener('click', onClick(qct));
+
+  sap.connect();
+  qct.connect();
 })();
