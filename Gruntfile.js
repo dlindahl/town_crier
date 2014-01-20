92d359ac410a6222eb2e807287f12c908a9618fd
Derek Lindahl <dlindahl@customink.com>
2014-01-20 11:25:14 -0800

    Adds uglify to build steps, licenses, and a banner to the distribution files

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,13 +11,41 @@ module.exports = function(grunt) {
     },
     jshint: {
       all: ['Gruntfile.js', 'src/*.js']
+    },
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
+                '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
+                '<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
+                '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
+                ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */\n'
+      },
+      minified: {
+        files: {
+          'dist/town_crier.min.js' : ['build/town_crier.js']
+        }
+      },
+      unminified: {
+        options: {
+          mangle : false,
+          beautify: {
+            beautify: true,
+            comments: true,
+            indent_level: 2,
+            width: 80
+          }
+        },
+        files: {
+          'dist/town_crier.js' : ['build/town_crier.js']
+        }
+      }
     }
   });
 
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-
   grunt.loadNpmTasks('grunt-browserify');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
 
   // Default task(s).
-  grunt.registerTask('default', ['jshint', 'browserify']);
+  grunt.registerTask('default', ['jshint', 'browserify', 'uglify']);
 };
--- dist/town_crier.js
+++ dist/town_crier.js
@@ -1,233 +1,139 @@
-(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-var ES      = window.EventSource,
-    semver  = require('./version'),
-    events  = require('./events'),
-    bind    = events.bind,
-    unbind  = events.unbind,
-    trigger = events.trigger;
-
-var DISCONNECTED = 0,
-    CONNECTING = 1,
-    CONNECTED = 2,
-    globalCfg = {
-      appName  : null,
-      hostname : '',
-      token    : '',
-      userId   : '',
-      retryInterval : 3000,
+/*! Town Crier - v0.0.1 - 2014-01-20
+* https://github.com/dlindahl/town_crier
+* Copyright (c) 2014 Derek Lindahl; Licensed MIT, GPL */
+!function e(t, n, r) {
+  function s(o, u) {
+    if (!n[o]) {
+      if (!t[o]) {
+        var a = "function" == typeof require && require;
+        if (!u && a) return a(o, !0);
+        if (i) return i(o, !0);
+        throw new Error("Cannot find module '" + o + "'");
+      }
+      var f = n[o] = {
+        exports: {}
+      };
+      t[o][0].call(f.exports, function(e) {
+        var n = t[o][1][e];
+        return s(n ? n : e);
+      }, f, f.exports, e, t, n, r);
+    }
+    return n[o].exports;
+  }
+  for (var i = "function" == typeof require && require, o = 0; o < r.length; o++) s(r[o]);
+  return s;
+}({
+  1: [ function(require, module) {
+    // Global config settings
+    function configure(options) {
+      return options ? (Object.keys(options).forEach(function(k) {
+        globalCfg[k] = options[k];
+      }), globalCfg) : globalCfg;
+    }
+    // Main TownCrier constructor
+    function TownCrier(options) {
+      Object.keys(globalCfg).forEach(function(k) {
+        "undefined" == typeof options[k] && (options[k] = globalCfg[k]);
+      }), this.options = options, this.state = DISCONNECTED;
+    }
+    function autoReconnect() {
+      this.trigger("reconnect", e, this), this.connect();
+    }
+    function onOpen(e) {
+      this.state = CONNECTED, this.trigger("open", e, this);
+    }
+    function onMessage() {
+      this.trigger("message", this);
+    }
+    function onError(e) {
+      var srcEvent = e.currentTarget;
+      srcEvent.readyState === ES.CLOSED ? this.state == CONNECTING ? // Enable auto reconnect in browsers that don't support it (i.e. FF 26.0)
+      setTimeout(autoReconnect.bind(this), this.options.retryInterval) : this.disconnect() : srcEvent.readyState === ES.CONNECTING ? (this.state = CONNECTING, 
+      this.trigger("reconnect", e, this)) : this.trigger("error", e, this);
+    }
+    function onClose() {
+      this.trigger("close", this);
+    }
+    function connect() {
+      var host = this.options.hostname, url = host + "/firehose/" + this.options.appName;
+      return this.options.token && (url += "?token=" + this.options.token), this.options.bindings.forEach(function(binding) {
+        if ("" === binding.exchange) throw new Error('Invalid Exchange name: "' + binding.exchange + '"');
+        url += "&exchanges[]=" + binding.exchange, url += "&routingKeys[]=" + binding.routingKey;
+      }), url += "&userId=" + this.options.userId, this._onOpen = onOpen.bind(this), this._onMessage = onMessage.bind(this), 
+      this._onError = onError.bind(this), this._onClose = onClose.bind(this), this.state = CONNECTING, 
+      this.source = new ES(url), this.source.addEventListener("open", this._onOpen, !1), 
+      this.source.addEventListener("message", this._onOpen, !1), this.source.addEventListener("error", this._onError, !1), 
+      this.source.addEventListener("close", this._onClose, !1), this;
+    }
+    function disconnect() {
+      return this.source.close(), this._onOpen && (this.source.removeEventListener("open", this._onOpen), 
+      delete this._onOpen), this._onMessage && (this.source.removeEventListener("message", this._onMessage), 
+      delete this._onMessage), this._onError && (this.source.removeEventListener("error", this._onError), 
+      delete this._onError), this._onClose && (this.source.removeEventListener("close", this._onClose), 
+      delete this._onClose), delete this.source, this.state = DISCONNECTED, this.trigger("close", this), 
+      this;
+    }
+    var ES = window.EventSource, semver = require("./version"), events = require("./events"), bind = events.bind, unbind = events.unbind, trigger = events.trigger, DISCONNECTED = 0, CONNECTING = 1, CONNECTED = 2, globalCfg = {
+      appName: null,
+      hostname: "",
+      token: "",
+      userId: "",
+      retryInterval: 3e3
     };
-
-// Global config settings
-function configure(options) {
-  if(!options) return globalCfg;
-
-  Object.keys(options).forEach(function(k) {
-    globalCfg[k] = options[k];
-  });
-
-  return globalCfg;
-}
-
-// Main TownCrier constructor
-function TownCrier(options) {
-  Object.keys(globalCfg).forEach(function(k) {
-    if(typeof options[k] === 'undefined') options[k] = globalCfg[k];
-  });
-
-  this.options = options;
-  this.state = DISCONNECTED;
-}
-
-TownCrier.__defineGetter__('config', function() {
-  return Object.create(globalCfg);
-});
-
-function autoReconnect() {
-  this.trigger('reconnect', e, this);
-  this.connect();
-}
-
-function onOpen(e) {
-  this.state = CONNECTED;
-  this.trigger('open', e, this);
-}
-
-function onMessage() {
-  this.trigger('message', this);
-}
-
-function onError(e) {
-  var srcEvent = e.currentTarget;
-
-  if (srcEvent.readyState === ES.CLOSED) {
-    if(this.state == CONNECTING) {
-      // Enable auto reconnect in browsers that don't support it (i.e. FF 26.0)
-      setTimeout(autoReconnect.bind(this), this.options.retryInterval);
-    } else {
-      this.disconnect();
+    TownCrier.__defineGetter__("config", function() {
+      return Object.create(globalCfg);
+    });
+    // Define the public API
+    var TC = TownCrier;
+    TC.configure = configure, TC.VERSION = semver, TC.prototype = {
+      connect: connect,
+      disconnect: disconnect,
+      trigger: trigger,
+      bind: bind,
+      unbind: unbind,
+      on: bind,
+      off: unbind
+    }, module.exports = TC;
+  }, {
+    "./events": 2,
+    "./version": 4
+  } ],
+  2: [ function(require, module) {
+    function bind(event, callback) {
+      var events = event.split(" "), cbs = this._callbacks || (this._callbacks = {});
+      return events.forEach(function(name) {
+        cbs[name] || (cbs[name] = []), cbs[name].push(callback);
+      }), this;
     }
-  } else if (srcEvent.readyState === ES.CONNECTING) {
-    this.state = CONNECTING;
-    this.trigger('reconnect', e, this);
-  } else {
-    this.trigger('error', e, this);
-  }
-}
-
-function onClose() {
-  this.trigger('close', this);
-}
-
-function connect() {
-  var host = this.options.hostname,
-      url = host+'/firehose/'+this.options.appName;
-
-  if(this.options.token) url += '?token='+this.options.token;
-
-  this.options.bindings.forEach(function(binding) {
-    if(binding.exchange === '') {
-      throw new Error('Invalid Exchange name: "'+binding.exchange+'"');
+    function unbind(ev, callback) {
+      if (0 === arguments.length) return this._callbacks = {}, this;
+      if (!ev) return this;
+      var list, evs = ev.split(" ");
+      return evs.forEach(function(name) {
+        return (list = this._callbacks[name]) ? callback ? void list.forEach(function(cb, i) {
+          return cb ? cb !== callback ? !0 : (list = list.slice(), list.splice(i, 1), this._callbacks = list, 
+          !1) : !0;
+        }.bind(this)) : (delete this._callbacks[name], this) : this;
+      }.bind(this)), this;
     }
-    url += '&exchanges[]=' + binding.exchange;
-    url += '&routingKeys[]=' + binding.routingKey;
-  });
-  url += '&userId=' + this.options.userId;
-
-  this._onOpen    = onOpen.bind(this);
-  this._onMessage = onMessage.bind(this);
-  this._onError   = onError.bind(this);
-  this._onClose   = onClose.bind(this);
-
-  this.state = CONNECTING;
-
-  this.source = new ES(url);
-
-  this.source.addEventListener('open',    this._onOpen,  false);
-  this.source.addEventListener('message', this._onOpen,  false);
-  this.source.addEventListener('error',   this._onError, false);
-  this.source.addEventListener('close',   this._onClose, false);
-
-  return this;
-}
-
-function disconnect() {
-  this.source.close();
-
-  if(this._onOpen) {
-    this.source.removeEventListener('open', this._onOpen);
-    delete this._onOpen;
-  }
-  if(this._onMessage) {
-    this.source.removeEventListener('message', this._onMessage);
-    delete this._onMessage;
-  }
-  if(this._onError) {
-    this.source.removeEventListener('error', this._onError);
-    delete this._onError;
-  }
-  if(this._onClose) {
-    this.source.removeEventListener('close', this._onClose);
-    delete this._onClose;
-  }
-
-  delete this.source;
-
-  this.state = DISCONNECTED;
-
-  this.trigger('close', this);
-
-  return this;
-}
-
-// Define the public API
-var TC = TownCrier;
-TC.configure = configure;
-TC.VERSION = semver;
-TC.prototype = {
-  connect    : connect,
-  disconnect : disconnect,
-  trigger    : trigger,
-  bind       : bind,
-  unbind     : unbind,
-  on         : bind,
-  off        : unbind
-};
-
-module.exports = TC;
-},{"./events":2,"./version":4}],2:[function(require,module,exports){
-function bind(event, callback) {
-  var name,
-      events = event.split(' '),
-      cbs = this._callbacks || (this._callbacks = {});
-
-  events.forEach(function(name) {
-    cbs[name] || (cbs[name] = []);
-    cbs[name].push(callback);
-  });
-
-  return this;
-}
-
-function unbind(ev, callback) {
-  if(arguments.length === 0) {
-    this._callbacks = {};
-    return this;
-  }
-
-  if(!ev) return this;
-
-  var list,
-      evs = ev.split(' ');
-
-  evs.forEach(function(name) {
-    list = this._callbacks[name];
-
-    if(!list) return this;
-
-    if(!callback) {
-      delete this._callbacks[name];
-      return this;
+    function trigger() {
+      var args = 0 === arguments.length ? [] : Array.prototype.slice.call(arguments, 0), ev = args.shift(), list = this._callbacks[ev];
+      if (list) return list.forEach(function(callback) {
+        return callback.apply(this, args) === !1 ? !1 : void 0;
+      }), !0;
     }
-
-    list.forEach(function(cb, i) {
-      if(!cb) return true;
-
-      if(cb !== callback) return true;
-
-      list = list.slice();
-      list.splice(i, 1);
-      this._callbacks = list;
-
-      return false;
-    }.bind(this));
-  }.bind(this));
-
-  return this;
-}
-
-function trigger() {
-  var args = arguments.length === 0 ? [] : Array.prototype.slice.call(arguments, 0),
-      ev = args.shift(),
-      list = this._callbacks[ev];
-
-  if(!list) return;
-
-  list.forEach(function(callback) {
-    if(callback.apply(this, args) === false) {
-      return false;
-    }
-  });
-
-  return true;
-}
-
-module.exports = {
-  bind    : bind,
-  unbind  : unbind,
-  trigger : trigger
-};
-},{}],3:[function(require,module,exports){
-window.TownCrier = require('./client');
-},{"./client":1}],4:[function(require,module,exports){
-module.exports = '0.0.1';
-},{}]},{},[3])
+    module.exports = {
+      bind: bind,
+      unbind: unbind,
+      trigger: trigger
+    };
+  }, {} ],
+  3: [ function(require) {
+    window.TownCrier = require("./client");
+  }, {
+    "./client": 1
+  } ],
+  4: [ function(require, module) {
+    module.exports = "0.0.1";
+  }, {} ]
+}, {}, [ 3 ]);
