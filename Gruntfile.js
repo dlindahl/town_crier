d298d7e02bf33b6b75fe48009ac64c52252ee506
Derek Lindahl <dlindahl@customink.com>
2014-01-21 13:22:06 -0800

    Refactors the Gruntfile to make it a bit more readable

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,50 +1,51 @@
 module.exports = function(grunt) {
   grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
+    pkg: grunt.file.readJSON('package.json')
+  });
 
-    browserify: {
-      dist: {
-        files: {
-          'build/town_crier.js': ['src/town_crier.js'],
-        }
-      }
-    },
-    jshint: {
-      all: ['Gruntfile.js', 'src/*.js']
-    },
-    uglify: {
-      options: {
-        banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
-                '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
-                '<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
-                '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
-                ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */\n'
-      },
-      minified: {
-        files: {
-          'dist/town_crier.min.js' : ['build/town_crier.js']
-        }
-      },
-      unminified: {
-        options: {
-          mangle : false,
-          beautify: {
-            beautify: true,
-            comments: true,
-            indent_level: 2,
-            width: 80
-          }
-        },
-        files: {
-          'dist/town_crier.js' : ['build/town_crier.js']
-        }
+  grunt.loadNpmTasks('grunt-browserify');
+  grunt.config('browserify', {
+    dist: {
+      files: {
+        'build/<%= pkg.name %>.js': ['src/<%= pkg.name %>.js'],
       }
     }
   });
 
-  grunt.loadNpmTasks('grunt-browserify');
   grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.config('jshint', {
+    all: ['Gruntfile.js', 'src/*.js']
+  });
+
   grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.config('uglify', {
+    options: {
+      banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
+              '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
+              '<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
+              '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
+              ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */\n'
+    },
+    minified: {
+      files: {
+        'dist/<%= pkg.name %>.min.js' : ['build/<%= pkg.name %>.js']
+      }
+    },
+    unminified: {
+      options: {
+        mangle : false,
+        beautify: {
+          beautify: true,
+          comments: true,
+          indent_level: 2,
+          width: 80
+        }
+      },
+      files: {
+        'dist/<%= pkg.name %>.js' : ['build/<%= pkg.name %>.js']
+      }
+    }
+  });
 
   // Default task(s).
   grunt.registerTask('default', ['jshint', 'browserify', 'uglify']);
