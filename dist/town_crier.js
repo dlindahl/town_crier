753471ae304136683edca94395d4a234dc12b457
Derek Lindahl <dlindahl@customink.com>
2014-01-29 13:50:52 -0800

    Version bump to 0.0.7

--- dist/town_crier.js
+++ dist/town_crier.js
@@ -1,4 +1,4 @@
-/*! Town Crier - v0.0.6 - 2014-01-27
+/*! Town Crier - v0.0.7 - 2014-01-29
 * https://github.com/dlindahl/town_crier
 * Copyright (c) 2014 Derek Lindahl; Licensed MIT, GPL */
 !function e(t, n, r) {
